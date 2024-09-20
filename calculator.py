d16d7d445941ce634b9c5140ac0d38d10baa0944
Kshithi7 <raokshithi9@gmail.com>
2024-09-20 19:51:35 +0530

    simple calculator

--- calculator.py
+++ calculator.py
@@ -1,20 +1,26 @@
-def add(a,b):
+
+# Define the arthmetic operations
+def add(a,b):  # addition
     return a+b
-def sub(a,b):
+def sub(a,b):  # subtraction
     return a-b
-def mul(a,b):
+def mul(a,b):  # multiplication
     return a*b
-def div(a,b):
+def div(a,b):   # division
     if b==0:
-     return "divisor cannot be zero"
+     return "divisor cannot be zero" # mathematical error
     return a/b
 
+
+# Display in-app messages
 print("Welcome to simple calculator!")
 print("perform desired operation")
 
+# User input
 x = float(input("Enter first number:"))
 y = float(input("Enter second number:"))
 
+# Display the menu
 print("Enter operation to be performed:")
 print("1 Addition")
 print("2 Subtraction")
@@ -22,6 +28,7 @@ print("3 Multiplication")
 print("4 Division")
 op = input ("Your choice: ")
 
+# Call the  desired defined functions 
 if op == "1":
   answer= add(x,y)
 elif op == "2":
@@ -33,4 +40,5 @@ elif op == "4":
 else: 
   answer="Operation not found!"
 
+# print the answer
 print("Answer:",answer)
